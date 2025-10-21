f6ddfae1cab88eaa6887a74b55ffb3fee6435d3e
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-10-21 22:50:20 +0200

    feat: Add CityResponseModel and ReceiptResponseModel with JSON serialization

---
+++ lib/features/store/shipment/data/models/receipt_response_model.dart
@@ -0,0 +1,62 @@
+import 'package:fares/features/driver/orders/data/models/parcels_response_model.dart';
+import 'package:json_annotation/json_annotation.dart';
+part 'receipt_response_model.g.dart';
+
+@JsonSerializable(checked: true)
+class ReceiptResponseModel {
+  final ReceiptDataModel data;
+
+  ReceiptResponseModel({required this.data});
+
+  factory ReceiptResponseModel.fromJson(Map<String, dynamic> json) =>
+      _$ReceiptResponseModelFromJson(json);
+}
+
+@JsonSerializable(checked: true)
+class ReceiptDataModel {
+  @JsonKey(name: 'current_page')
+  final int currentPage;
+  @JsonKey(name: 'last_page')
+  final int lastPage;
+  final List<ReceiptModel> data;
+
+  ReceiptDataModel({
+    required this.currentPage,
+    required this.lastPage,
+    required this.data,
+  });
+
+  factory ReceiptDataModel.fromJson(Map<String, dynamic> json) =>
+      _$ReceiptDataModelFromJson(json);
+}
+
+@JsonSerializable(checked: true)
+class ReceiptModel {
+  final int id;
+  @JsonKey(name: 'created_at')
+  final String createdAt;
+  @JsonKey(name: 'total_parcels_products_count')
+  final int parcelsCount;
+  @JsonKey(name: 'total_products_price')
+  final String totalPrice;
+  @JsonKey(name: 'total_shipping_price')
+  final String shippingPrice;
+  @JsonKey(name: 'total_withdrawal_transaction')
+  final String? withdrawalTransaction;
+  final String total;
+  final List<ParcelModel>? parcels;
+
+  ReceiptModel({
+    required this.id,
+    required this.createdAt,
+    required this.parcelsCount,
+    required this.totalPrice,
+    required this.shippingPrice,
+    this.withdrawalTransaction,
+    required this.total,
+    this.parcels,
+  });
+
+  factory ReceiptModel.fromJson(Map<String, dynamic> json) =>
+      _$ReceiptModelFromJson(json);
+}
