fed6d3d00491112205146fed367548e265667269
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-11-11 19:14:00 +0200

    Remove unused SVG assets and update image references in app_images.dart

--- lib/core/utils/app_images.dart
+++ lib/core/utils/app_images.dart
@@ -6,13 +6,9 @@ class AppImages {
 
   static const String imagesAddSequre = "assets/images/add_sequre.svg";
 
-  static const String imagesAllOrders = "assets/images/all_orders.svg";
-
   static const String imagesAndroid12Splash = "assets/images/android12_splash.png";
 
   static const String imagesAppLogo = "assets/images/app_logo.png";
-
-  static const String imagesAreDelivery = "assets/images/are_delivery.svg";
 
   static const String imagesCanceled = "assets/images/canceled.svg";
 
@@ -24,19 +20,13 @@ class AppImages {
 
   static const String imagesClosed = "assets/images/closed.png";
 
-  static const String imagesCompanyDues = "assets/images/company_dues.svg";
-
   static const String imagesComplaints = "assets/images/complaints.svg";
 
   static const String imagesConnectingNow = "assets/images/connecting_now.png";
 
-  static const String imagesConversations = "assets/images/conversations.svg";
-
   static const String imagesDelay = "assets/images/delay.png";
 
   static const String imagesDelivered = "assets/images/delivered.png";
-
-  static const String imagesDelivery = "assets/images/delivery.svg";
 
   static const String imagesDeposit = "assets/images/deposit.svg";
 
@@ -61,8 +51,6 @@ class AppImages {
   static const String imagesIconPrimary = "assets/images/icon-primary.png";
 
   static const String imagesIconWhite = "assets/images/icon-white.png";
-
-  static const String imagesInDelivery = "assets/images/in_delivery.svg";
 
   static const String imagesLogoFullDark = "assets/images/logo-full-dark.png";
 
