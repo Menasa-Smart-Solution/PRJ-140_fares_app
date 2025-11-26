7c00563550a6713e8efa498dd8f6ec79d8be6753
AbdoAlHady <20912018100332@fci.zu.edu.eg>
2025-11-27 00:56:48 +0200

    feat: Integrate Firebase and local notifications, update iOS project configuration
    - Added Podfile.lock to manage dependencies including Firebase and local notifications.
    - Updated Runner.xcodeproj to include Pods and framework references for Firebase and local notifications.
    - Modified AppDelegate.swift to configure Firebase and set up local notifications.
    - Updated Info.plist to include necessary permissions for camera and photo library access.
    - Created Runner.entitlements for push notifications.
    - Updated environment variables in env_variables.g.dart.
    - Adjusted macOS Flutter configuration files to include Pods support.
    - Created a new Podfile for macOS to manage dependencies.

--- ios/Runner/AppDelegate.swift
+++ ios/Runner/AppDelegate.swift
@@ -1,5 +1,8 @@
 import Flutter
 import UIKit
+import flutter_local_notifications
+import UserNotifications
+import FirebaseCore
 
 @main
 @objc class AppDelegate: FlutterAppDelegate {
@@ -9,10 +12,12 @@ import UIKit
   ) -> Bool {
     GeneratedPluginRegistrant.register(with: self)
     return super.application(application, didFinishLaunchingWithOptions: launchOptions)
-    FirebaseApp.configure()
-     FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
-    GeneratedPluginRegistrant.register(with: registry)
-  }
+      FirebaseApp.configure()
+
+    FlutterLocalNotificationsPlugin.setPluginRegistrantCallback { (registry) in
+        GeneratedPluginRegistrant.register(with: registry)
+    }
+
 
     if #available(iOS 10.0, *) {
   UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
